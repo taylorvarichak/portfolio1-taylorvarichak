d0ce1f0deab1c4bbb42b99e8f61311b02e68ef5b
taylorvarichak <136833577+taylorvarichak@users.noreply.github.com>
2023-08-01 13:27:58 -0400

    added js toggle menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Welcome to my Porfolio!");
+$(".nav-toggle-button").on("click", function () {
+    const nav = $("nav");
+    if (nav.hasClass("opened")) {
+        nav.removeClass("opened")
+    } else {
+        nav.addClass("opened")
+    }
+})
